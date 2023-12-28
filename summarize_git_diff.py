9414e951f20285c99fc061eafb021090b162ff18
Rose Goldfarb <rosegoldfarb@Roses-Laptop.local>
2023-12-27 20:20:33 -0500

    update testing file

--- summarize_git_diff.py
+++ summarize_git_diff.py
@@ -1,7 +1,6 @@
 import argparse
 import subprocess
 from openai import OpenAI
-import os
 from dotenv import load_dotenv
 
 
@@ -11,27 +10,24 @@ def get_git_diff(file_path):
     return result.stdout
 
 def summarize_text (text):
-    # move to different method
-    load_dotenv()
-    # api_key = os.environ.get("OPENAI_API_KEY")
-
     client = OpenAI()
-    messages = [ {"role": "system", "content": "You are a code reviewer that provides a concise and simple summary of the changes found in a Git diff file."}]
+
+    messages = [ {"role": "system", "content": "You are a code reviewer that provides a concise and simple summary of the changes found in a Git diff file. Provide the summary in bullet format."}]
     changes = text.split('\n\n')
+
     for change in changes:
-        # didn't work
-        clean_change = change.strip().replace('\\n', '\n')
-        messages.append({"role": "user", "content": clean_change})
+        messages.append({"role": "user", "content": change})
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
     
-    return completion.choices[0].message
+    return completion.choices[0].message.content
 
 def main():
-    print("running main meth")
+    load_dotenv()
+
     parser = argparse.ArgumentParser(description="Summarize Git diff file using OpenAI API.")
     parser.add_argument("file_path", type=str, help="Path to the Git diff file.")
     args = parser.parse_args()
--- tester.py
+++ tester.py
@@ -1,5 +1,19 @@
-tasks = []
+from datetime import datetime
 
-def add_task(task):
-    tasks.append(task)
+def get_user_birthdate():
+    birthdate_str =  input("Enter your date of birth in the form YYYY-MM-DD \n")
+    return datetime.strptime(birthdate_str, '%Y-%m-%d')
 
+def get_age_in_days(birthdate):
+     current_date = datetime.now()
+     age_in_days = (current_date - birthdate).days
+     return age_in_days
+
+def main():
+    user_birth_date = get_user_birthdate()
+    age_in_days = get_age_in_days(user_birth_date)
+    print(age_in_days)
+    
+ 
+if __name__ == "__main__":
+    main()
